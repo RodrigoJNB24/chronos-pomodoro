e0daffbff8b102a12ac6446bd0ec6c4f74fa8296
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-04-08 14:35:53 -0300

    14. importando_css_modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-export function App(){
-    console.log('oi')
-    return (
-      <>
-        <h1>Olá Mundo</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam,
-          temporibus sunt fugit autem illo, ad harum corporis eaque quod
-          veritatis cum. Ab debitis culpa, et omnis minus fugit est. Dicta?
-        </p>
-      </>
-    );
+import { Heading } from './components/Heading';
+
+import './style/theme.css';
+import './style/global.css';
+
+export function App() {
+  return (
+    <>
+      <Heading />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam,
+        temporibus sunt fugit autem illo, ad harum corporis eaque quod veritatis
+        cum. Ab debitis culpa, et omnis minus fugit est. Dicta?
+      </p>
+    </>
+  );
 }
