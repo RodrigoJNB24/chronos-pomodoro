132769bc2b815fd8507e9ddc4095b222ca87f864
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-04-09 18:13:22 -0300

    22. Organizando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 import './style/theme.css';
 import './style/global.css';
+
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola mundo 1
-        <button>
-          <TimerIcon/>
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam,
-        temporibus sunt fugit autem illo, ad harum corporis eaque quod veritatis
-        cum. Ab debitis culpa, et omnis minus fugit est. Dicta?
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
