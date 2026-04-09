9dd2a978d671bed3307cd89f8d08550cbee579ef
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-04-09 17:07:11 -0300

    Finalizacao do componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 import './style/theme.css';
 import './style/global.css';
@@ -6,7 +7,12 @@ import './style/global.css';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Ola mundo 1
+        <button>
+          <TimerIcon/>
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam,
         temporibus sunt fugit autem illo, ad harum corporis eaque quod veritatis
