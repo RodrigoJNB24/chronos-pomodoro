5a6f835ef15a8c75eb7ea4f80ddb83ca8cd66029
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-04-10 20:13:25 -0300

    25. Componente Menu e Component CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown'
+
 import './style/theme.css';
 import './style/global.css';
-
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
