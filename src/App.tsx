96197496f6cb38f2c76c5a59dfa8b219d9c72f6d
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-04-17 10:48:14 -0300

    49. Context + State

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
+import { Home } from './pages/Home';
+import { TaskContextProvider } from './Contexts/TaskContext';
+
 import './style/theme.css';
 import './style/global.css';
-import { Home } from './pages/Home';
-import { TaskStateModel } from './models/TaskStateModel';
-import { useState } from 'react';
-
-const initialState: TaskStateModel = {
-  task: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '05:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    LongBreakTime: 15
-  } 
-}
 
 export function App() {
-  const [state, setState] = useState(initialState);
-  console.log('APP', state);
-  return <Home/>;
+
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,11 +2,23 @@ import { DefaultInput } from '../DefaultInput';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { useTaskContext } from '../../Contexts/TaskContext';
 
 export function MainForm() {
 
+  const { setState } = useTaskContext()
+
+  function handleCliclk(){
+    setState(prevState => {
+      return {
+        ...prevState,
+        formattedSecondsRemaining: '21:00'
+      }
+    })
+  }
   return (
     <form className='form' action=''>
+      <button type="button" onClick={handleCliclk}>Clicar</button>
       <div className='formRow'>
         <DefaultInput
           id='meuInput'
