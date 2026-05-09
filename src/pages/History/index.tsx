53363d75f28dadeb3b5731132a8cea38cb953ae5
RodrigoJNB24 <rodrigoborgesfilho2@gmail.com>
2026-05-09 14:47:48 -0300

    Add caixa de dialogo na page History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,7 +9,9 @@ import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
 import { sortTasks, sortTasksOptions } from '../../utils/sortTasks';
 import { useEffect, useState } from 'react';
+import { showMessage } from '../../adapters/showMessage';
 import { TaskActionTypes } from '../../Contexts/TaskContext/taskAction';
+
 import styles from './styles.module.css';
 
 export type HomeProps = {
@@ -19,6 +21,7 @@ export type HomeProps = {
 
 export function History() {
   const { state, dispatch } = useTaskContext();
+  const hasTasks = state.tasks.length > 0;
   const [sortTaskOptions, setSortTaskOptions] = useState<sortTasksOptions>(
     () => {
       return {
@@ -51,14 +54,14 @@ export function History() {
         direction: prevState.direction,
         field: prevState.field,
       }),
-    }))
-  }, [state.tasks])
+    }));
+  }, [state.tasks]);
 
   function handleResetHistory() {
-    if(!confirm('Tem certeza que deseja apagar o historico')) return;
-    
-    dispatch({ type: TaskActionTypes.RESET_STATE })
-    
+    showMessage.confirm('Deseja apagar todo o historico?', confirmation => {
+      if (!confirmation) return;
+      dispatch({ type: TaskActionTypes.RESET_STATE });
+    });
   }
 
   return (
@@ -66,67 +69,78 @@ export function History() {
       <Container>
         <Heading>
           <span>History</span>
-          <span className={styles.buttonContainer}>
-            <DefaultButton
-              onClick={() => handleResetHistory()}
-              icon={<TrashIcon />}
-              color='red'
-              aria-label='Apagar todo historico'
-              title='Apagar historico'
-            />
-          </span>
+          {hasTasks && (
+            <span className={styles.buttonContainer}>
+              <DefaultButton
+                onClick={() => handleResetHistory()}
+                icon={<TrashIcon />}
+                color='red'
+                aria-label='Apagar todo historico'
+                title='Apagar historico'
+              />
+            </span>
+          )}
         </Heading>
       </Container>
 
       <Container>
-        <div className={styles.responsiveTable}>
-          <table>
-            <thead>
-              <tr>
-                <th
-                  onClick={() => handleSortTask({ field: 'name' })}
-                  className={styles.thSort}
-                >
-                  Tarefa ↕
-                </th>
-                <th
-                  onClick={() => handleSortTask({ field: 'durationInMinutes' })}
-                  className={styles.thSort}
-                >
-                  Duração ↕
-                </th>
-                <th
-                  onClick={() => handleSortTask({ field: 'startDate' })}
-                  className={styles.thSort}
-                >
-                  Data ↕
-                </th>
-                <th>Status</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
+        {hasTasks && (
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th
+                    onClick={() => handleSortTask({ field: 'name' })}
+                    className={styles.thSort}
+                  >
+                    Tarefa ↕
+                  </th>
+                  <th
+                    onClick={() =>
+                      handleSortTask({ field: 'durationInMinutes' })
+                    }
+                    className={styles.thSort}
+                  >
+                    Duração ↕
+                  </th>
+                  <th
+                    onClick={() => handleSortTask({ field: 'startDate' })}
+                    className={styles.thSort}
+                  >
+                    Data ↕
+                  </th>
+                  <th>Status</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {sortTaskOptions.tasks.map(task => {
-                const taskTypeDictionary = {
-                  workTime: 'Foco',
-                  shortBreakTime: 'Descanso curto',
-                  longBreakTime: 'Descanso longo',
-                };
+              <tbody>
+                {sortTaskOptions.tasks.map(task => {
+                  const taskTypeDictionary = {
+                    workTime: 'Foco',
+                    shortBreakTime: 'Descanso curto',
+                    longBreakTime: 'Descanso longo',
+                  };
 
-                return (
-                  <tr key={task.id}>
-                    <td>{task.name}</td>
-                    <td>{task.durationInMinutes}min</td>
-                    <td>{formatDate(task.startDate)}</td>
-                    <td>{getTaskStatus(task, state.activeTask)}</td>
-                    <td>{taskTypeDictionary[task.type]}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+                  return (
+                    <tr key={task.id}>
+                      <td>{task.name}</td>
+                      <td>{task.durationInMinutes}min</td>
+                      <td>{formatDate(task.startDate)}</td>
+                      <td>{getTaskStatus(task, state.activeTask)}</td>
+                      <td>{taskTypeDictionary[task.type]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {!hasTasks && (
+          <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
+            Inicie uma tarefa para mostrar o Historico
+          </p>
+        )}
       </Container>
     </MainTemplate>
   );
